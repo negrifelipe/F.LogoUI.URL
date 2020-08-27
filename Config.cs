a297c5801083e7a25a01af07542046a521aefab7
Feli <64453816+DarkTequila22@users.noreply.github.com>
2020-08-27 18:56:15 -0300

    1.0.0
    Plugin v1.0.0

---
+++ Config.cs
@@ -0,0 +1,18 @@
+﻿using Rocket.API;
+
+namespace F.LogoUI
+{
+    public class Config : IRocketPluginConfiguration
+    {
+        public ushort LogoUI;
+        public string LogoURL;
+        public bool LogoEnbled;
+
+        public void LoadDefaults()
+        {
+            LogoUI = (ushort)32410;
+            LogoURL = "https://i.imgur.com/0M2kAwf.png";
+            LogoEnbled = true;
+        }
+    }
+}
---
+++ Main.cs
@@ -0,0 +1,46 @@
+﻿using Rocket.Core.Plugins;
+using Rocket.Unturned;
+using Rocket.Unturned.Player;
+using SDG.Unturned;
+using System;
+
+using Logger = Rocket.Core.Logging.Logger;
+
+namespace F.LogoUI
+{
+    public class Main : RocketPlugin<Config>
+    {
+        public static Main Instance;
+        protected override void Load()
+        {
+            Instance = this;
+
+            Logger.Log("####################################################", ConsoleColor.Red);
+            Logger.Log("#                 F.LogoUI Loaded                  #", ConsoleColor.Red);
+            Logger.Log("#                 Plugin By: Feli                  #", ConsoleColor.Red);
+            Logger.Log("#    Discord Support:https://discord.gg/6zQVJ9p    #", ConsoleColor.Red);
+            Logger.Log("#                 Plugin Ver: 1.0.0                #", ConsoleColor.Red);
+            Logger.Log("####################################################", ConsoleColor.Red);
+            U.Events.OnPlayerConnected += Events_OnPlayerConnected;
+        }
+
+        private void Events_OnPlayerConnected(UnturnedPlayer player)
+        {
+            if(this.Configuration.Instance.LogoEnbled == false)
+            {
+                return;
+            }
+            else
+            {
+                EffectManager.sendUIEffect(this.Configuration.Instance.LogoUI, (short)32411, player.CSteamID, true);
+                EffectManager.sendUIEffectImageURL(32411, player.CSteamID, true, "Image", this.Configuration.Instance.LogoURL);
+            }
+        }
+
+        protected override void Unload()
+        {
+            U.Events.OnPlayerConnected -= Events_OnPlayerConnected;
+            Instance = null;
+        }
+    }
+}
